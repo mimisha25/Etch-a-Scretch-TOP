f8d7124fbf6ad31a86c51475c76d27107490db82
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-04-30 15:29:12 -0400

    Feat add elements thorugh DOM and append them to body

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const h1 = document.createElement("h1");
 h1.innerText="Etch-a-Sketch";
 document.body.appendChild(h1);
+
+const section = document.createElement("section");
+document.body.appendChild(section);
+
+const btnContainer = document.createElement("div");
+btnContainer.classList.add("button-container");
+section.appendChild(btnContainer);
+
+const container = document.createElement("div");
+container.classList.add("container");
