b46124d8c5bda89998eef0b6f96f6e3b0954121f
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-05-01 15:32:43 -0400

    Apply for loop to create buttons

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ section.appendChild(btnContainer);
 
 const container = document.createElement("div");
 container.classList.add("container");
+
+for (let i=0; i<6; i++){
+    const btnArrays=["btn16", "btn8", "rainbow", "grey", "erase", "reset"];
+    const objArrays = Object.assign({},btnArrays);
+    const btnTextArrays=["Grid 16x16", "Grid 8x8", "Rainbow", "Grey", "Erase", "Reset"];
+    btnArrays[i]= document.createElement("button");
+    btnArrays[i].innerText=`"`+btnTextArrays[i]+`"`;
+    btnArrays[i].setAttribute("id", objArrays[i]);
+    btnContainer.appendChild(btnArrays[i])
+    }
